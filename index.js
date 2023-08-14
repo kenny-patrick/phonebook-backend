ab3c96155118c1af36136096540ddeee1462b6ff
Kenny Patrick <kennypatrick96@gmail.com>
2023-08-14 13:39:13 -0600

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -10,6 +11,8 @@ morgan.token('body', (req, res) => {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use(cors())
 
 let persons = [
   { 
